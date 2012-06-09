a68b7f76df53241824913d8f3c26e3e5a8f9cc8c
Joachim Breitner <mail@joachim-breitner.de>
2012-06-09 18:00:12 +0200

    Fix findNearestBlockAt

--- common/map.py
+++ common/map.py
@@ -47,8 +47,8 @@ class Map:
 		if position.y < 0 or position.y >= self.blocks_vertical * TILE_HEIGHT: return result
 
 		minDist = -1
-		ix = int(position.x/TILE_WIDTH)
-		iy = int(position.y/TILE_HEIGHT)
+		ix = int(position.x/TILE_WIDTH + 0.5)
+		iy = int(position.y/TILE_HEIGHT + 0.5)
 		for x in range(ix, ix+1):
 			for y in range(iy, iy+1):
 				block = list[x][y]
