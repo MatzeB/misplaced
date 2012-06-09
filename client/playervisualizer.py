767552f92e8567b350156b377b29f6299e35ba24
Joachim Breitner <mail@joachim-breitner.de>
2012-06-09 19:10:58 +0200

    Dummy visualization for stunned player

--- client/playervisualizer.py
+++ client/playervisualizer.py
@@ -68,7 +68,10 @@ class PlayerVisualizer(Visualizer):
 				self.carriedblockvisualizer.draw(offset, obj, rotation=self.getDirectionRotation(obj))
 
 			# move player image...
-			Visualizer.draw(self, offset + Vector(16,5), obj, rotation=0)#self.getDirectionRotation(obj))
+			rot = 0
+			if obj.stunned:
+				rot = 180
+			Visualizer.draw(self, offset + Vector(16,5), obj, rotation=rot)#self.getDirectionRotation(obj))
 
 			targetpos = obj.position + Vector(16,16) + dirvector * 16 + offset
 
